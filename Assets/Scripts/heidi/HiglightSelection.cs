d3eb0267ae9d0786bc9385a86e427669aec6354a
Heidi28 <64629568+Heidi28@users.noreply.github.com>
2021-04-27 14:01:01 +0930

    Add minor fixes
    Change lab background
    Adjust position of pathways, buttons and avatars
    Add post process volume (not working)
    Set SteamVR bindings to be default to work when built as .exe

--- Assets/Scripts/heidi/HiglightSelection.cs
+++ Assets/Scripts/heidi/HiglightSelection.cs
@@ -44,7 +44,6 @@ public class HiglightSelection : MonoBehaviour
         }
         else
         {
-
             var grabObject = other.gameObject.GetComponent<Grabbable>();
             grabObject.touchCount--;
 
